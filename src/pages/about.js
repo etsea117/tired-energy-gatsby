9e3462a7372a296978b989055a0ab8b7c900714d
Ethan Clatterbaugh <ethan.sea17@gmail.com>
2022-05-28 16:25:29 -0600

    Adding components, about page, and author image

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return(
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm a product manager, with 4 years of experience in the solar and energy storage industry.
+            I am neurodiverse and narcoleptic, and this blog is about human experiences in the renewables field.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
