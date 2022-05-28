732457f8e63cf7ce24f90cf1deff868899c6c10c
Ethan Clatterbaugh <ethan.sea17@gmail.com>
2022-05-28 17:55:55 -0600

    Adding markdown blog functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,52 @@ module.exports = {
       options: {
         name: `blog`,
         path: `${__dirname}/blog`
-      }
-    }
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 650,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: '›',
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false,
+              },
+              escapeEntities: {},
+            },
+          },
+        ],
+      },
+    },
   ],
 }
